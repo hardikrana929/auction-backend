eca7aad43c9d076701a592f9f313db0418ce81fb
Asavala Hardik <hardikrana929@gmail.com>
2026-09-05 16:42:53 +0530

    change deployment changes

--- server.js
+++ server.js
@@ -72,12 +72,4 @@ const io = initializeSocket(
 
 app.set("io", io);
 
-
-
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-    console.log('Server running successful...');
-})
-
 module.exports = app;
