cbee1edf5d70fd366397b230111ef21c7c7dc8ed
Asavala Hardik <hardikrana929@gmail.com>
2026-09-06 19:27:38 +0530

    change user

--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         enum: ['user', 'admin'],
-        default: 'user'
+        default: 'admin'
     },
     isActive: {
         type: Boolean,
