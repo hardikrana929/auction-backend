3b72d597a6f3ede056ad79494bc76638671c5e40
Asavala Hardik <hardikrana929@gmail.com>
2026-09-06 19:32:27 +0530

    change user role

--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         enum: ['user', 'admin'],
-        default: 'admin'
+        default: 'user'
     },
     isActive: {
         type: Boolean,
